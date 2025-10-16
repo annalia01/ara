c69d2d7f4aaca4ac24ebe12601786a31c139e7fa
annalia01 <annalia.ruggiero01@gmail.com>
2025-10-16 15:27:07 +0200

    Update and rename apps/fmatmul_2/fmatmul.h to apps/fmatmul_2/inc/fmatmul.h

--- apps/fmatmul_2/inc/fmatmul.h
+++ apps/fmatmul_2/inc/fmatmul.h
@@ -30,13 +30,7 @@ void fmatmul_4x4(float *c, const float *a, const float *b,
 void fmatmul_vec_4x4_slice_init();
 void fmatmul_vec_4x4(float *c, const float *a, const float *b,
                      unsigned long int n, unsigned long int p);
-void fmatmul_uint8(uint8_t *c, const uint8_t *a, const uint8_t *b, unsigned long int m,
-             unsigned long int n, unsigned long int p);
 
-void fmatmul_4x4_uint8(uint8_t *c, const uint8_t *a, const uint8_t *b,
-                 unsigned long int m, unsigned long int n, unsigned long int p);
-void fmatmul_vec_4x4_uint8(uint8_t *c, const uint8_t *a, const uint8_t *b,
-                     unsigned long int n, unsigned long int p);
 
 #define DELTA 0.000001
 
